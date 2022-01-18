91b590bb644d77320bcffd01d2865a5e718316d8
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    First commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("HelloWorld bean1 equal to HelloWorld bean2 ? " + (bean1==bean2));
+        System.out.println("Cat bean1 equal to Cat bean2 ? " + (catBean1==catBean2));
     }
 }
