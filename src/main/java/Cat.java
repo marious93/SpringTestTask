6cdc977cb3636cc5b4aeb51e3b263ff678cc91ef
marious93 <marious_93@mail.ru>
2025-10-30 16:08:45 +1000

    First commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    private String name;
+
+    public Cat() {}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
